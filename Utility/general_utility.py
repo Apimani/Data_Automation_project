a81ae24aa166cdb9bd22dd5d4cb06e9963d613f3
ksreenivasulu443 <ksreenivasulu443>
2024-04-09 16:11:01 +0000

    updated

--- Utility/general_utility.py
+++ Utility/general_utility.py
@@ -3,23 +3,29 @@ import os
 from pyspark.sql.types import StructType
 
 # Given absolute path
-given_path = os.path.abspath(os.path.dirname(__file__))
+#given_path = os.path.abspath(os.path.dirname(__file__))
+os.environ.setdefault("project_path", os.getcwd())
+given_path = os.environ.get("project_path")
+print(given_path)
 
 
 def read_config(database):
     parent_path = os.path.dirname(given_path) + '\Config\Config.json'
+
     # Read the JSON configuration file
     with open(parent_path) as f:
         config = json.load(f)[database]
     return config
 
 def read_schema(schema_file_path):
-    path = os.path.dirname(given_path) + '/schema'+ '/' +schema_file_path
+    path = os.path.dirname(given_path) + r'/schema/' +schema_file_path
     # Read the JSON configuration file
     print(path)
-    with open(r'/Workspace/Repos/etlbigdataautomation@gmail.com/Data_Automation_project/schema/contact_info_schema.json', 'r') as schema_file:
+    with open(path, 'r') as schema_file:
         schema = StructType.fromJson(json.load(schema_file))
     return schema
+
+print(read_schema('contact_info_schema.json'))
 
 def fetch_source_file_path(file_path):
     path = os.path.dirname(given_path) + '/source_files/'+file_path
